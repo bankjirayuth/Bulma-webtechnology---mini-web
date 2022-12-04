2a4e5e25bd476aa5612a07ce91605a0b3ce433e4
bankjirayuth <53044682+bankjirayuth@users.noreply.github.com>
2022-12-04 23:04:32 +0700

    [ADD] first commit

---
+++ java.js
@@ -0,0 +1,15 @@
+
+//    var tag = document.createElement("p");
+//    var text = document.createTextNode("Tutorix is the best e-learning platform");
+//    tag.appendChild(text);
+//    var element = document.getElementById("new");
+//    element.appendChild(tag);
+
+//    let tag = document.createElement("p");
+//    let text = document.createTextNode(<div class="columns">
+//    <div class="column">1</div>
+//    <div class="column">2</div>
+//    <div class="column">3</div>
+//    <div class="column">4</div>
+//    <div class="column">5</div>
+//  </div>)
